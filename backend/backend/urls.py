77653b3e922a426c44f32c36b49befacf956db0f
allen-haitao <hw.waikato@gmail.com>
2024-09-13 10:05:08 +1200

    handle 404

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({"message": "This is a backend API, no frontend available."})
 
 urlpatterns = [
+    path('', home),  
     path("admin/", admin.site.urls),
     path('', include('finance.urls')),
 ]
