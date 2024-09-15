3845288ebf9c7119f09a6354ab5b420d18b8b952
allen-haitao <hw.waikato@gmail.com>
2024-09-15 20:18:58 +1200

    adjust

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -7,8 +7,7 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div className="footer-content">
-                <p>&copy; {currentYear} PFM. All rights reserved.</p>
-                <p>
+                <p>&copy; {currentYear} PFM. All rights reserved.
                     <a href="/privacy-policy">Privacy Policy</a> |
                     <a href="/terms-of-service"> Terms of Service</a>
                 </p>
